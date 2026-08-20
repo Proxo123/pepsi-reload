17bcbbc6c1f5a9ffb820991536b3b398a5a91eca
Proxo123 <Proxo123@users.noreply.github.com>
2026-08-20 09:54:13 -0500

    Pin loader to v23 commit 83e7005

--- loader.lua
+++ loader.lua
@@ -11,7 +11,7 @@ if getgenv()._PepsiReloadUnload then
 end
 
 -- Pinned commit so Roblox HttpGet does not serve stale branch cache
-local COMMIT = "1a002b2"
+local COMMIT = "83e7005"
 local REPO = "https://raw.githubusercontent.com/Proxo123/pepsi-reload/" .. COMMIT .. "/"
 
 getgenv().PEPSI_REPO = REPO
